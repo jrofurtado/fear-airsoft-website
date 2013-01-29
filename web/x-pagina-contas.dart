a13eeb2c0f2cb75bf9db017e1e80ab579086a4b1
= <=>
2013-01-29 16:18:26 -0100

    gitignore

--- web/x-pagina-contas.dart
+++ web/x-pagina-contas.dart
@@ -5,5 +5,11 @@ class PaginaContas extends WebComponent {
   String graficoSaldo = "https://docs.google.com/a/fear-airsoft.com/spreadsheet/oimg?key=0AoNj2G61OMg-dDhVbFJWbDRoMVFZeDFCMGFiS21FSmc&oid=7&zx=7a5nciaim3lc";
   String graficoCustos = "https://docs.google.com/a/fear-airsoft.com/spreadsheet/oimg?key=0AoNj2G61OMg-dDhVbFJWbDRoMVFZeDFCMGFiS21FSmc&oid=5&zx=9p9kcll4dk9";
   String graficoBalanco = "https://docs.google.com/a/fear-airsoft.com/spreadsheet/oimg?key=0AoNj2G61OMg-dDhVbFJWbDRoMVFZeDFCMGFiS21FSmc&oid=3&zx=1yuvo8fjfe0m";
+  
+  void inserted() {
+    js.scoped(){
+      
+    }
+  }
 }
 
