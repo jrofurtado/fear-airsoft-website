d4ccd3e8cdcd5e7043bb97612b452db08e0c9fe2
= <=>
2013-03-07 17:13:18 -0100

    Refactoring

--- web/x-jogo.dart
+++ web/x-jogo.dart
@@ -2,55 +2,54 @@ import 'package:web_ui/web_ui.dart';
 import 'package:js/js.dart' as js;
 import 'package:google_maps/js_wrap.dart' as jsw;
 import 'package:google_maps/google_maps.dart';
-import 'package:google_calendar_v3_api/calendar_v3_api_browser.dart' as calendarclient;
 import 'dart:html';
 import 'dart:async';
 import 'model.dart';
 
 class Jogo extends WebComponent {
   String get temperaturaClass{
-    if(double.parse(model.tempo[0]["tempMinC"])<5) {
+    if(double.parse(model.jogo[0]["tempo"]["tempMinC"])<5) {
       return "red";
     }
-    if(double.parse(model.tempo[0]["tempMaxC"])>=35) {
+    if(double.parse(model.jogo[0]["tempo"]["tempMaxC"])>=35) {
       return "red";
     }
-    if(double.parse(model.tempo[0]["tempMinC"])<10) {
+    if(double.parse(model.jogo[0]["tempo"]["tempMinC"])<10) {
       return "yellow";
     }
-    if(double.parse(model.tempo[0]["tempMaxC"])>=30) {
+    if(double.parse(model.jogo[0]["tempo"]["tempMaxC"])>=30) {
       return "yellow";
     }
     return "";
   }
   String get precipitacaoClass{
-    if(double.parse(model.tempo[0]["precipMM"])>=4) { //Chuva intensa (http://wiki.sandaysoft.com/a/Rain_measurement#Standard_.28graduated_cylinder.29Rain_Gauge)
+    if(double.parse(model.jogo[0]["tempo"]["precipMM"])>=4) { //Chuva intensa (http://wiki.sandaysoft.com/a/Rain_measurement#Standard_.28graduated_cylinder.29Rain_Gauge)
         return "red";
     }
-    if(double.parse(model.tempo[0]["precipMM"])>=1) { //Chuva moderada
+    if(double.parse(model.jogo[0]["tempo"]["precipMM"])>=1) { //Chuva moderada
       return "yellow";
     }
     return "";
   }
   String get ventoClass{
-    if(double.parse(model.tempo[0]["windspeedKmph"])>=45) { //Vento intenso (http://library.thinkquest.org/C003603/english/hurricanes/hurricanewind.shtml)
+    if(double.parse(model.jogo[0]["tempo"]["windspeedKmph"])>=45) { //Vento intenso (http://library.thinkquest.org/C003603/english/hurricanes/hurricanewind.shtml)
         return "red";
     }
-    if(double.parse(model.tempo[0]["windspeedKmph"])>=25) { //Vento moderado
+    if(double.parse(model.jogo[0]["tempo"]["windspeedKmph"])>=25) { //Vento moderado
       return "yellow";
     }
     return "";
   }
-  List<calendarclient.EventAttendee> get participantesSim{
-    var res = model.participantes.where((participante) => (participante.responseStatus=="accepted"));
+  List<Map> get participantesSim{
+    var res = model.jogo[0]["participantes"].where((participante) => (participante["responseStatus"]=="accepted"));
     return res.toList();
   }
-  List<calendarclient.EventAttendee> get participantesNao{
-    var res = model.participantes.where((participante) => (participante.responseStatus=="declined"));
+  List<Map> get participantesNao{
+    var res = model.jogo[0]["participantes"].where((participante) => (participante["responseStatus"]=="declined"));
     return res.toList();
   }
-  List<calendarclient.EventAttendee> get participantesTalvez{
-    var res = model.participantes.where((participante) => (participante.responseStatus=="tentative"));
+  List<Map> get participantesTalvez{
+    var res = model.jogo[0]["participantes"].where((participante) => (participante["responseStatus"]=="tentative"));
     return res.toList();
   }
 
--- web/x-participante.dart
+++ web/x-participante.dart
@@ -1,15 +1,6 @@
 import 'package:web_ui/web_ui.dart';
-import 'package:google_calendar_v3_api/calendar_v3_api_browser.dart' as calendarclient;
 import 'model.dart';
 
 class Participante extends WebComponent {
-  calendarclient.EventAttendee participante;
-  String get foto{
-    var res = model.membros.where((membro) => (membro["email"]==participante.email));
-    if(res.length>0) {
-      return "${Model.endpoint_imagens}/Fotos/100x100/${res.first["socio"]}.jpg";
-    } else {
-      return "${Model.endpoint_imagens}/Fotos/no_photo_100x100.jpg";
-    }
-  }
+  Map participante;
 }
